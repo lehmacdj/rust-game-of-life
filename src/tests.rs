fd5e8422abfa80b06a0413fc21086765eea6b9c1
Devin Lehmacher <lehmacdj@gmail.com>
2017-01-02 03:36:34 -0500

    Add some more tests

--- src/tests.rs
+++ src/tests.rs
@@ -36,7 +36,7 @@ fn frame_next() {
 }
 
 #[test]
-fn game_of_life() {
+fn game_of_life_oscillator() {
     use rules::GOLState;
     use rules::GOLState::Alive;
 
@@ -61,4 +61,51 @@ fn game_of_life() {
 
     assert_eq!(frame2, expected);
     assert_eq!(frame3, frame1);
+}
+
+#[test]
+fn game_of_life_glider() {
+    use rules::GOLState;
+    use rules::GOLState::Alive;
+
+    // Create a glider as pictured and follow its transformations:
+    // DADDD
+    // DDADD
+    // AAADD
+    // DDDDD
+    // DDDDD
+    let mut init = frame::Frame::<GOLState>::new(5, 5);
+    init.set(1, 0, Alive);
+    init.set(2, 1, Alive);
+    init.set(0, 2, Alive);
+    init.set(1, 2, Alive);
+    init.set(2, 2, Alive);
+
+    let mut second = frame::Frame::<GOLState>::new(5, 5);
+    second.set(0, 1, Alive);
+    second.set(2, 1, Alive);
+    second.set(1, 2, Alive);
+    second.set(2, 2, Alive);
+    second.set(1, 3, Alive);
+
+    let next = init.next_frame(rules::game_of_life);
+
+    assert_eq!(next, second);
+}
+
+#[test]
+fn game_of_life_still_square() {
+    use rules::GOLState;
+    use rules::GOLState::Alive;
+
+    let mut frame = frame::Frame::<GOLState>::new(3, 3);
+    frame.set(0, 0, Alive);
+    frame.set(0, 1, Alive);
+    frame.set(1, 0, Alive);
+    frame.set(0, 0, Alive);
+    frame.set(1, 1, Alive);
+
+    let frame_new = frame.next_frame(rules::game_of_life);
+
+    assert_eq!(frame, frame_new);
 }
