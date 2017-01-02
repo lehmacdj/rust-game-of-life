b2cc11352268202bf9459f5a822a4f9bc3772d4a
Devin Lehmacher <lehmacdj@gmail.com>
2017-01-02 03:41:23 -0500

    Add a little bit of documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #[cfg(test)]
 mod tests;
 
+/// Contains functions that deal with handling individual frames of a simulation
 pub mod frame;
+/// Contains functions that represent rulesets for simulations.
+/// These generally consist of one data type and one function that transforms
+/// any given square to a new square based on the surrounding region of tiles.
 pub mod rules;
